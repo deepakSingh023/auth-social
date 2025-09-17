c1383ccaa99877f0580a3d55b8a5d55db073aec9
deepakSingh023 <dsinghrana71@gmail.com>
2025-09-17 15:20:51 +0530

    login fix

--- src/main/java/com/example/auth_social/AuthSocialApplication.java
+++ src/main/java/com/example/auth_social/AuthSocialApplication.java
@@ -2,12 +2,14 @@ package com.example.auth_social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   // ✅ this enables @Scheduled tasks like KeepAliveTask
 public class AuthSocialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthSocialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSocialApplication.class, args);
+    }
 
 }
