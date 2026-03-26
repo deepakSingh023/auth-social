0afa5e3c1fb01bc26638bb00c7c7718c26e2a611
deepakSingh023 <dsinghrana71@gmail.com>
2026-03-26 16:50:31 +0530

    minor fix

--- src/main/java/com/example/auth_social/tasks/FeignClient.java
+++ src/main/java/com/example/auth_social/tasks/FeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@org.springframework.cloud.openfeign.FeignClient(name="profile-creation" ,url="https://profile-social-cokq.onrender.com/api")
+@org.springframework.cloud.openfeign.FeignClient(name="profile-creation" ,url="${PROFILE_SERVICE}")
 public interface FeignClient {
 
 
